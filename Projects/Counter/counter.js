7f1bdd40a87c66e3c39a67d1832d147adbf43a08
Burak Gün <burak.gun@hotmail.com>
2022-09-12 15:56:51 +0300

    JS

--- Projects/Counter/counter.js
+++ Projects/Counter/counter.js
@@ -1,7 +1,7 @@
 let timeElapsed = 0;
 let timeElapsedSec = 0;
 let timeElapsedMin = 0;
-let timeElapsedHour = 0; 
+let timeElapsedHour = 0;
 let counterState = 0;
 
 let clock = document.getElementById("clock");
@@ -11,78 +11,117 @@ let clock = document.getElementById("clock");
 // document.getElementById("clock").innerHTML = "burakbaba" ;
 
 let start = document.querySelector(".start");
-let stopp  = document.querySelector(".stop");
+let stopp = document.querySelector(".stop");
 let reset = document.querySelector(".reset");
+let set = document.querySelector(".set");
 
-
-function stopCounter(){
+function stopCounter() {
     clearInterval(interval);
 }
 
 var time = 1000;
-function startCounter(){
+function startCounter() {
     counterState = 1;
     // clock.textContent = 0 ;
-    interval = setInterval(increaseCounter, time);
+    // interval = setInterval(increaseCounter, time);
+    interval = setInterval(decreaseCounter, time);
 
 }
 
-function addZeroBefore(n){
+function addZeroBefore(n) {
     return (n < 10 ? '0' : '') + n;
 }
 
-function showTime(){
-    clock.textContent = "Time Elapsed " + addZeroBefore(timeElapsedHour) +":"+ addZeroBefore(timeElapsedMin)+":"+addZeroBefore(timeElapsedSec);
+function showTime() {
+    clock.textContent = "Time Elapsed " + (timeElapsedHour) + ":" + (timeElapsedMin) + ":" + (timeElapsedSec);
+
+    //clock.textContent = "Time Elapsed " + addZeroBefore(timeElapsedHour) +":"+ addZeroBefore(timeElapsedMin)+":"+addZeroBefore(timeElapsedSec);
 }
 
-function resetCounter(){
+function resetCounter() {
     timeElapsed = 0;
     timeElapsedSec = 0;
     timeElapsedMin = 0;
-    timeElapsedHour = 0; 
+    timeElapsedHour = 0;
 
     showTime();
 }
 
 
-function increaseCounter(){
+function increaseCounter() {
 
-    
-    timeElapsedSec++ ;
 
-    if(timeElapsedSec == 60){
-        timeElapsedSec=0;
+    timeElapsedSec++;
+    // set fonksiyonunda bu değerlerden daha büyük değer girilmesi durumunda 0 olarak atayacak
+    if (timeElapsedSec >= 60) {
+        timeElapsedSec = 0;
         timeElapsedMin++;
     }
- 
-    if(timeElapsedMin == 60){
-        timeElapsedMin=0;
+
+    if (timeElapsedMin >= 60) {
+        timeElapsedMin = 0;
         timeElapsedHour++;
     }
-    if(timeElapsedHour == 24){
-        timeElapsedHour=0;
+    if (timeElapsedHour >= 24) {
+        timeElapsedHour = 0;
     }
-       
+
     showTime();
-    
+
+}
+
+
+function decreaseCounter() {
+
+    if (timeElapsedSec < 0) timeElapsedSec = 60;
+
+    timeElapsedSec--;
+    // set fonksiyonunda bu değerlerden daha büyük değer girilmesi durumunda 0 olarak atayacak
+    if (timeElapsedSec < 0) {
+        timeElapsedSec = 59;
+        timeElapsedMin--;
+    }
+
+    if (timeElapsedMin < 0) {
+        timeElapsedMin = 59;
+        timeElapsedHour--;
+    }
+    if (timeElapsedHour < 0) {
+        timeElapsedHour = 23;
+    }
+
+    showTime();
 }
 
 
 
 start.addEventListener("click", () => {
-    
-    if(counterState == 0){
+
+    if (counterState == 0) {
         startCounter();
     }
 });
 
 stopp.addEventListener("click", () => {
     stopCounter();
-    counterState=0;
+    counterState = 0;
 });
 
 reset.addEventListener("click", () => {
     stopCounter();
     resetCounter();
     counterState = 0;
+});
+
+set.addEventListener("click", () => {
+    var name = window.prompt("Enter Value");
+    const timeArray = name.split(":");
+
+    timeElapsedSec = timeArray[2];
+    timeElapsedMin = timeArray[1];
+    timeElapsedHour = timeArray[0];
+
+    showTime();
+
+
 });
