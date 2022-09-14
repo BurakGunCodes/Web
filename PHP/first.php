7bb6b29b127e48ab063376b4d23cecfdd7306e4f
Burak Gün <burak.gun@hotmail.com>
2022-09-14 14:19:08 +0300

    PHP

---
+++ PHP/first.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Example</title>
+</head>
+<body>
+    <?php
+    $name = "jo" ;
+    $age = 06;
+    
+    echo "<h1> $name $age  yaşındadır.  </h1>";
+    echo "<hr>" ;
+
+    $myString = "This is string";
+    $myInt = 33;
+    $myFloat = 90.5;
+    $myBool = TRUE;
+    echo "$myString $myInt $myFloat $myBool";
+
+    ?>
+
+</body>
+</html>
+
+<!-- 
+    video link : https://www.youtube.com/watch?v=OK_JCtrrv-c
+-->
